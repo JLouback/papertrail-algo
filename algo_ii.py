423a18f96c189aa70943065977cb4d76f9cc9829
hex <hex@localhost.localdomain>
2015-12-13 21:50:51 -0500

    Faster

--- algo_ii.py
+++ algo_ii.py
@@ -3,38 +3,64 @@ __author__ = 'julianalouback'
 import csv
 import sys
 
-csv.field_size_limit(sys.maxsize)
+csv.field_size_limit( 2**30 )   # was sys.maxsize
+
+# Threshold for relevance of keywords
+THRESHOLD = 0.6
+
+keywords_docsrels = {}
+
+def populate_iks_dict():
+    with open( 'keywordscores.csv', 'r' ) as iks_file:
+        reader = csv.reader( iks_file, delimiter=',', quotechar='\"' )
+        for row in reader:
+            docsrels = row[1].split(';')
+            del docsrels[-1]
+            keywords_docsrels[ row[0] ] = docsrels
+
+"""
+def get_relevance2( keywords ):
+    # NOTE: What if we see a keyword (in 'keywords') that isn't in the dict?
+    # We ignore it. (For now, at least.)
+    all_scores = {}
+    for keyword in keywords:
+        if keyword in keywords_docsrels:
+            all_scores[ keyword ] = keywords_docsrels[ keyword ]
+    return all_scores
 
 # For a list of keywords, returns a dict with keyword as key and list of doc:relevance_score values
 def get_relevance(keywords):
-    data = open('keywordscores.csv', 'r')
-    reader = csv.reader(data, delimiter=',', quotechar='\"')
     counter = 0
     all_scores = {}
-    for row in reader:
-        kw = row[0]
-        if kw in keywords:
-            counter += 1
-            scores = row[1].split(';')
-            del scores[-1]
-            all_scores[kw] = scores
-            if counter == len(keywords):
-                break
+    with open('keywordscores.csv', 'r') as data:
+        reader = csv.reader(data, delimiter=',', quotechar='\"')
+        for row in reader:
+            kw = row[0]
+            if kw in keywords:
+                counter += 1
+                scores = row[1].split(';')
+                del scores[-1]
+                all_scores[kw] = scores
+                if counter == len(keywords):
+                    break
     return all_scores
+"""
 
 def get_scores(keywords, authorities):
+    # NOTE: What if we see a keyword (in 'keywords') that isn't in the dict?
+    # We ignore it. (For now, at least.)
     docs = {}
-    all_scores = get_relevance(keywords)
     # Add doc relevance scores to doc entries
-    for keyword in all_scores.keys():
-        scores = all_scores[keyword]
-        for entry in scores:
-            doc = entry.split(':')[0]
-            score = entry.split(':')[1]
-            try:
-                docs[doc] = docs[doc] + float(score)
-            except KeyError:
-                docs[doc] = float(score)
+    for keyword in keywords:
+        if keyword in keywords_docsrels:
+            scores = keywords_docsrels[keyword]
+            for entry in scores:
+                doc = entry.split(':')[0]
+                score = entry.split(':')[1]
+                try:
+                    docs[doc] = docs[doc] + float(score)
+                except KeyError:
+                    docs[doc] = float(score)
     # Add authority score to doc entries, remove docs with 0 authority
     for doc in docs.keys():
         try:
@@ -49,14 +75,19 @@ def get_scores(keywords, authorities):
     return top
 
 
-def predict_citations(index, candidates, authorities):
-    threshold = 0.6
+def predict_citations( candidates, authorities ):
     keywords = []
     for candidate in candidates:
-        keyword = candidate.split(':')[0]
-        relevance = float(candidate.split(':')[1])
-        # Ignore keywords less than threshold
-        if relevance < threshold:
+        # TODO: We should probably drop keywords that contain ':'
+	colon = candidate.rfind(':')
+	keyword = candidate[:colon]
+	# NOTE: Some keywords don't have a score, e.g. ID 747316's "durp" keyw
+	try:
+	    relevance = float( candidate[colon+1:] )
+        except ValueError:
+            break
+        # Ignore doc_ids less than threshold
+        if relevance < THRESHOLD:
             break
         keywords.append(keyword)
     # dictionary of top k recommended documents and a score
